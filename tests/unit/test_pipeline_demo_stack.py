54a5961fb9fcc3ce46c9d22bcfac96e887b54bf4
Derrick Choo <8960762+dalacan@users.noreply.github.com>
2023-06-30 10:40:45 +1000

    Initial commit

---
+++ tests/unit/test_pipeline_demo_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from pipeline_demo.pipeline_demo_stack import PipelineDemoStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in pipeline_demo/pipeline_demo_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = PipelineDemoStack(app, "pipeline-demo")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
